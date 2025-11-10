b87f9447f5e86f6f2fc2e0f9394700e57f443c7a
tzf230201 <tzf230201@gmail.com>
2025-11-10 17:58:30 +0900

    try outside folder

--- Algo2/algo2-slidev/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import slidev from '@slidev/cli/vite'
-
-export default defineConfig({
-  plugins: [slidev()],
-  base: '/TEUKU_zikri/algo2/',   // ← ini penting!
-})
