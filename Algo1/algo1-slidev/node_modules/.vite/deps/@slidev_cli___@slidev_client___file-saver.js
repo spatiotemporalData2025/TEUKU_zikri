8313b4cc0f4d05ee1803b8af340358f611d7250c
tzf230201 <tzf230201@gmail.com>
2025-11-10 16:12:46 +0900

    fix: force vite base path for GitHub Pages

--- Algo1/algo1-slidev/node_modules/.vite/deps/@slidev_cli___@slidev_client___file-saver.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_FileSaver_min
-} from "./chunk-2KYM5DNG.js";
-import "./chunk-PR4QN5HX.js";
-export default require_FileSaver_min();
--- Algo1/algo1-slidev/node_modules/.vite/deps/@slidev_cli___@slidev_client___recordrtc.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_RecordRTC
-} from "./chunk-VVNBZMWG.js";
-import "./chunk-PR4QN5HX.js";
-export default require_RecordRTC();
