0c36d41db1f76e5600a17b3a37f310c7c12c6ea9
tzf230201 <tzf230201@gmail.com>
2025-11-10 16:05:56 +0900

    fix config slidev

--- Algo1/algo1-slidev/slidev.config.ts
+++ Algo1/algo1-slidev/slidev.config.ts
@@ -1,5 +1,18 @@
-export default {
-  title: 'Algorithm 1 Presentation: Language Learning',
+import { defineConfig } from 'slidev'
+
+export default defineConfig({
+  title: 'Algorithm 1 Presentation',
   theme: 'seriph',
   base: '/TEUKU_zikri/Algo1/',
-}
+  vite: {
+    build: {
+      outDir: '../../dist_Algo1',
+      assetsDir: 'assets',
+    },
+    server: {
+      fs: {
+        strict: false,
+      },
+    },
+  },
+})
--- Algo2/algo2-slidev/slidev.config.ts
+++ Algo2/algo2-slidev/slidev.config.ts
@@ -1,5 +1,18 @@
-export default {
+import { defineConfig } from 'slidev'
+
+export default defineConfig({
   title: 'Algorithm 2 Presentation: Behavioral Robot',
   theme: 'seriph',
   base: '/TEUKU_zikri/Algo2/',
-}
+  vite: {
+    build: {
+      outDir: '../../dist_Algo2',
+      assetsDir: 'assets',
+    },
+    server: {
+      fs: {
+        strict: false,
+      },
+    },
+  },
+})
